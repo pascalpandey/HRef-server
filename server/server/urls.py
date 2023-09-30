aec67ee96980f8fe23c0f800d4247cd77f2f13b6
pascal pandey <pascalis.pandey@gmail.com>
2023-10-01 01:11:14 +0800

    hehe

--- server/server/urls.py
+++ server/server/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('candidates/', include('candidates.urls'))
+    # path('candidates/', include('candidates.urls'))
 ]
